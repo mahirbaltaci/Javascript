bfb0f889b7acffa7593677956b7883d2205bbd38
Mahir BALTACI <mahir@Mahir-Air.local>
2016-04-07 21:17:08 +0300

    Düzenleme

--- select_il_ilce/iller.js
+++ select_il_ilce/iller.js
@@ -1,12 +1,12 @@
 // iller ve bağlı bilgileri arrayı oluşturuluyor. Valla hepsini yazamadım şimdilik. İnternette bulunur. :)
 var ilListesi = [
-  {"adi": "Adana",          "plaka": 01,  "ilce": ["Aladağ", "Ceyhan", "Çukurova", "Feke", "İmamoğlu", "Karaisalı", "Karataş", "Kozan", "Pozantı", "Saimbeyli", "Sarıçam", "Seyhan", "Tufanbeyli", "Yumurtalık", "Yüreğir"]},
-  {"adi": "Adıyaman",       "plaka": 02,  "ilce": ["Besni", "Çelikhan", "Gerger", "Gölbaşı", "Kahta", "Samsat", "Sincik", "Tut"]},
-  {"adi": "Afyonkarahisar", "plaka": 03,  "ilce": ["Başmakçı", "Bayat", "Bolvadin", "Çay", "Çobanlar", "Dazkırı", "Dinar", "Emirdağ", "Evciler", "Hocalar", "İhsaniye", "İscehisar", "Kızılören", "Sandıklı", "Sİnanpaşa", "Sultandağı", "Şuhut"]},
-  {"adi": "Antalya",        "plaka": 07,  "ilce": ["Manavgat", "Kaş", "Side", "Kemer", "Alanya"]},
-  {"adi": "Ankara",         "plaka": 06,  "ilce": ["Polatlı","Haymana", "Çankaya", "Gölbaşı"]},
-  {"adi": "İstanbul",       "plaka": 34,  "ilce": ["Beşiktaş", "Esenler", "Mecidiyeköy", "Kadıköy"]},
-  {"adi": "Kars",           "plaka": 36,  "ilce": ["Göle", "Aralık", "Posof", "Digor"]}
+  {"adi": "Adana",          "ilce": ["Aladağ", "Ceyhan", "Çukurova", "Feke", "İmamoğlu", "Karaisalı", "Karataş", "Kozan", "Pozantı", "Saimbeyli", "Sarıçam", "Seyhan", "Tufanbeyli", "Yumurtalık", "Yüreğir"]},
+  {"adi": "Adıyaman",       "ilce": ["Besni", "Çelikhan", "Gerger", "Gölbaşı", "Kahta", "Samsat", "Sincik", "Tut"]},
+  {"adi": "Afyonkarahisar", "ilce": ["Başmakçı", "Bayat", "Bolvadin", "Çay", "Çobanlar", "Dazkırı", "Dinar", "Emirdağ", "Evciler", "Hocalar", "İhsaniye", "İscehisar", "Kızılören", "Sandıklı", "Sİnanpaşa", "Sultandağı", "Şuhut"]},
+  {"adi": "Antalya",        "ilce": ["Manavgat", "Kaş", "Side", "Kemer", "Alanya"]},
+  {"adi": "Ankara",         "ilce": ["Polatlı","Haymana", "Çankaya", "Gölbaşı"]},
+  {"adi": "İstanbul",       "ilce": ["Beşiktaş", "Esenler", "Mecidiyeköy", "Kadıköy"]},
+  {"adi": "Kars",           "ilce": ["Göle", "Aralık", "Posof", "Digor"]}
 ];
 
 // iller selecti oluşturuluyor. Value olarak index değeri atanıyor.
